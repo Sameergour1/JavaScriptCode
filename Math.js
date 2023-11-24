8de40e865b4afa12879ade906415c8e10bbcc129
Md Sameer Gour <127622736+Sameergour1@users.noreply.github.com>
2023-11-24 20:57:17 +0000

    commit

---
+++ Math.js
@@ -0,0 +1,24 @@
+console.log(Math);
+console.log(Math.abs(-4));
+
+console.log(Math.abs(4));
+
+console.log(Math.round(4.3));
+console.log(Math.round(4.6));
+
+
+console.log(Math.random());
+
+//0-1
+
+console.log(Math.random()*10);
+
+
+//0-9
+
+console.log((Math.random()*10)+1);
+
+const min =10;
+const max =20;
+
+console.log((Math.random()*(max-min+1))+min);
---
+++ numberAndMath.js
@@ -0,0 +1,18 @@
+const score =500;
+const balance = new Number(500)
+console.log(balance);
+console.log(score);
+console.log(balance.toString());
+
+console.log(balance.toString().length);
+
+console.log(balance.toFixed(2));
+
+const otherNumber =123.8975;
+console.log(otherNumber.toPrecision(3));
+console.log(otherNumber.toPrecision(4));
+
+
+const hundreds =1000000
+console.log(hundreds.toLocaleString('en-In'));
+
