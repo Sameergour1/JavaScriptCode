c95eca100ea125c88ed1752d42888ad40cb7b5d3
Md Sameer Gour <127622736+Sameergour1@users.noreply.github.com>
2023-11-24 11:52:01 +0000

    commit

---
+++ String.js
@@ -0,0 +1,30 @@
+'sameer'
+"sameer"
+"hello"+"world";
+const name = "SAMEEER"
+const repoCount =50;
+console.log(name+repoCount+"value");
+
+
+// now new form of repersentation and uses of string 
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+//Declaring String 
+const gameName = new String (`sameer`);
+
+console.log(gameName[0]);
+
+console.log(gameName.length);
+
+console.log(gameName.toLocaleLowerCase());
+console.log(gameName.charAt(5));
+
+console.log(gameName.indexOf(`R`));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+const another =gameName.slice(1,4);
+
+console.log(another);
+
---
+++ TrimAndReplace.js
@@ -0,0 +1,15 @@
+const newString =  "                sameer";
+console.log(newString.trim());
+
+console.log(newString);
+
+
+const url = "https://sameer.com/sameer%20gour"
+
+console.log(url.replace(`%20`, `_`));
+
+console.log(url.includes(`sameer`));
+
+const gameName =new String (`sameer-gour-com`);
+console.log(gameName.split('-'));
+
